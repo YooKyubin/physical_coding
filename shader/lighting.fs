3002aa6c9ab5364a7f4698d0a7d503c05fb708db
YooKyubin <binde73@naver.com>
2022-12-31 12:40:32 +0900

    point light(attenuation)

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -9,7 +9,8 @@ out vec4 fragColor;
 uniform vec3 viewPos;
 
 struct Light {
-    vec3 direction;
+    vec3 position;
+	vec3 attenuation;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -24,21 +25,27 @@ struct Material {
 uniform Material material;
 
 void main() {
+    // abient
     vec3 texColor = texture(material.diffuse, texCoord).xyz;
 	vec3 ambient =  texColor * light.ambient;
  
-    // vec3 lightDir = normalize(light.position - position);
-    vec3 lightDir = normalize(-light.direction);
+    // diffuse
+    float dist = length(light.position - position);
+    vec3 distPoly = vec3(1.0, dist, dist*dist);
+    float attenuation = 1.0 / dot(distPoly, light.attenuation);
+
+    vec3 lightDir = (light.position - position) / dist;
     vec3 pixelNorm = normalize(normal);
     float diff = max(dot(pixelNorm, lightDir), 0.0);
     vec3 diffuse = diff * texColor * light.diffuse;
  
+    //specular
     vec3 specColor = texture(material.specular, texCoord).xyz;
     vec3 viewDir = normalize(viewPos - position);
     vec3 reflectDir = reflect(-lightDir, pixelNorm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = spec * specColor * light.specular;
 
-    vec3 result = ambient + diffuse + specular;
+    vec3 result = (ambient + diffuse + specular) * attenuation;
 	fragColor = vec4(result, 1.0);
 }
