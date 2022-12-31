5cce7cab5597c444c926ba0f2b2e2ab0688809ca
YooKyubin <binde73@naver.com>
2022-12-31 16:14:07 +0900

    spot light (경계선 interpolation적용)

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -11,7 +11,7 @@ uniform vec3 viewPos;
 struct Light {
     vec3 position;
     vec3 direction;
-    float cutoff;
+    vec2 cutoff;
 	vec3 attenuation;
     vec3 ambient;
     vec3 diffuse;
@@ -39,8 +39,11 @@ void main() {
     
     float theta = dot(lightDir, normalize(-light.direction));
     vec3 result = ambient;
+    float intensity = clamp(
+        (theta - light.cutoff[1]) / (light.cutoff[0] - light.cutoff[1]), 
+        0.0, 1.0);
 
-    if (theta > light.cutoff) {
+    if (intensity > 0.0) {
         vec3 pixelNorm = normalize(normal);
         float diff = max(dot(pixelNorm, lightDir), 0.0);
         vec3 diffuse = diff * texColor * light.diffuse;
@@ -52,7 +55,7 @@ void main() {
         float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
         vec3 specular = spec * specColor * light.specular;
 
-        result += diffuse + specular;
+        result += (diffuse + specular) * intensity;
     }
 
     result *= attenuation;
